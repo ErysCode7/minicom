84c9fec4efe4e47f93b2725f2180930fd02f51ec
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-28 21:46:13 +0800

    update footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,23 +1,17 @@
-import { ROUTES } from '@/utils/constant';
-import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 type Props = {};
 
-const Footer: NextPage<Props> = () => {
+const Footer = () => {
   const router = useRouter();
 
   const date = new Date().getFullYear();
 
   return (
-    <footer
-      className={`w-full h-20 shadow-sm bg-white ${
-        router.pathname !== ROUTES.PRODUCTS ? 'md:absolute bottom-0' : 'static'
-      } `}
-    >
+    <footer className={`w-full h-20 shadow-sm bg-white md:absolute bottom-0`}>
       <div className="flex items-center m-auto h-full w-[90%] lg:w-[85%]">
         <h2 className="text-gray-500 text-xs sm:text-sm">
-          Nextjs Template <span className="text-blue-500">©{date}.</span> All rights reserved
+          Minicom <span className="text-blue-500">©{date}.</span> All rights reserved
         </h2>
       </div>
     </footer>
