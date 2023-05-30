fea65f41d8294509be317fa145db503f8c1eec62
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-05-30 22:37:01 +0800

    added rating star package

--- src/modules/product-details/component/product-details.tsx
+++ src/modules/product-details/component/product-details.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import StarRatings from 'react-star-ratings';
 import { useHooks } from '../hooks';
 
 type Props = {};
@@ -7,6 +8,8 @@ const ProductDetails = (props: Props) => {
   const { productDetails, isLoadingProductDetails } = useHooks();
 
   console.log(productDetails);
+
+  const rate = Math.round(productDetails?.rating?.rate ?? 0) as number;
 
   if (isLoadingProductDetails) {
     return (
@@ -17,29 +20,26 @@ const ProductDetails = (props: Props) => {
   }
 
   return (
-    <div className="w-[90%] lg:w-[85%] m-auto flex flex-col laptop:flex-row items-center h-[calc(100vh_-_80px)]">
-      <div className="flex-1 relative w-[280px] h-[280px] mobile:w-full mobile:h-[300px] sm:h-[350px] laptop:w-[400px] laptop:h-[500px] rounded mt-5 laptop:mt-0">
-        <Image
-          src={productDetails?.image ?? ''}
-          alt={productDetails?.title ?? 'Image'}
-          fill
-          className="object-contain rounded"
-        />
+    <div className="w-[90%] lg:w-[85%] m-auto flex flex-col laptop:flex-row laptop:gap-10 items-center h-[calc(100vh_-_80px)]">
+      <div className="flex-1 relative rounded-xl w-[280px] h-[280px] mobile:w-full mobile:h-[300px] sm:h-[350px] laptop:w-[300px] laptop:h-[450px] mt-5 laptop:mt-0">
+        <Image src={productDetails?.image ?? ''} alt={productDetails?.title ?? 'Image'} fill />
       </div>
       <div className="flex-1 mt-5 pb-10 laptop:mt-0">
-        <p className="uppercase text-gray-400">{productDetails?.category}</p>
+        <p className="uppercase text-gray-400 text-sm">{productDetails?.category}</p>
         <h2 className="font-bold text-xl sm:text-2xl lg:text-4xl">{productDetails?.title}</h2>
-        <p className="text-gray-500 mt-3">{productDetails?.description}</p>
-        <div className="flex items-center gap-2 my-2">
-          <h4 className="text-base sm:text-xl">Category</h4>
-          <p className="capitalize text-base sm:text-xl">{productDetails?.category}</p>
+        <div>
+          <StarRatings
+            starDimension="20px"
+            starSpacing="2px"
+            rating={rate}
+            starRatedColor="rgb(34 197 94)"
+            numberOfStars={5}
+            name="rating"
+          />
+          <p>{rate}</p>
         </div>
-
+        <p className="text-gray-500 my-3">{productDetails?.description}</p>
         <p className="text-green-500 text-base sm:text-xl md:text-2xl">${productDetails?.price}</p>
-        <div className="mt-3">
-          <p>Count: {productDetails?.rating?.count}</p>
-          <p>Rate: {productDetails?.rating?.rate}</p>
-        </div>
       </div>
     </div>
   );
