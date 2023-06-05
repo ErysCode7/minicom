3193190ab28a12f3e3ac91514968652b63db16c2
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-06-05 22:19:27 +0800

    added cart context events

--- src/context/cart-context.tsx
+++ src/context/cart-context.tsx
@@ -9,21 +9,56 @@ type CartContextType = {
   // Define the properties and methods you want to provide in the context
   cart: Products[];
   setCart: Dispatch<SetStateAction<Products[]>>;
+  getItemsQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  clearCart: () => void;
 };
 
 export const CartContext = createContext<CartContextType>({
   cart: [],
   setCart: () => {},
+  getItemsQuantity: () => {
+    return 0;
+  },
+  increaseCartQuantity: () => {},
+  decreaseCartQuantity: () => {},
+  removeFromCart: () => {},
+  clearCart: () => {},
 });
 
 export const CartContextProvider = ({ children }: CartContextProps) => {
   const [cart, setCart] = useState<Products[]>([]);
 
+  //get the quantity of the product
+  const getItemsQuantity = () => {
+    return 0;
+  };
+
+  //increase product quantity in cart
+  const increaseCartQuantity = (id: number) => {};
+
+  //decrease product quantity in cart
+  const decreaseCartQuantity = (id: number) => {};
+
+  //remove from cart
+  const removeFromCart = (id: number) => {};
+
+  //clear cart
+  const clearCart = () => {};
+
   const value = {
     //state
     cart,
-    //func
+    //state func
     setCart,
+    //cart functions
+    getItemsQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+    clearCart,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
