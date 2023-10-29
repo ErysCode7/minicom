e32152965faf965f825d3a4905ed007a01e49872
Erys Mozo <mozoerys@gmail.com>
2023-10-29 23:37:42 +0800

    fix skeleton product loader

--- src/modules/products/components/product-loader-skeleton.tsx
+++ src/modules/products/components/product-loader-skeleton.tsx
@@ -4,15 +4,27 @@ import 'react-loading-skeleton/dist/skeleton.css';
 const ProductLoaderSkeleton = () => {
   return (
     <div className="flex justify-between gap-5 w-[90%] lg:w-[85%] m-auto my-10">
-      <div className="hidden sm:block w-48 flex-shrink-0">
-        <Skeleton count={12} />
+      <div>
+        <div className="hidden sm:flex w-48 flex-shrink-0 flex-col gap-5">
+          <Skeleton height={40} />
+          <Skeleton count={5} />
+          <Skeleton height={40} />
+          <Skeleton height={40} />
+        </div>
       </div>
-      <div className="w-full flex-grow">
-        <Skeleton
-          containerClassName="flex flex-wrap items-center justify-center laptop:justify-between gap-5"
-          count={12}
-          className="!h-[220px] !w-[200px] lg:!w-[180px] lg:!h-[180px] xl:!h-[200px] xl:!w-[200px]"
-        />
+      <div>
+        <div className="flex items-center gap-3 mb-3">
+          <Skeleton width={30} height={30} />
+          <Skeleton width={30} height={30} />
+          <Skeleton width={150} height={20} />
+        </div>
+        <div className="w-full flex-grow">
+          <Skeleton
+            containerClassName="flex flex-wrap items-center justify-center laptop:justify-between gap-5"
+            count={8}
+            className="!h-[220px] !w-[200px] lg:!w-[250px] lg:!h-[180px] xl:!h-[250px] xl:!w-[200px]"
+          />
+        </div>
       </div>
     </div>
   );
