7723de3905343ff3eef104b2b4d3dc216ddb84d7
Erys Mozo <mozoerys@gmail.com>
2023-10-30 22:30:22 +0800

    create hooks for nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,16 @@
 import Layout from '@/components/layout/layout';
+import useNProgress from '@/hooks/use-nprogress';
 import '@/styles/globals.css';
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
 
-  NProgress.configure({
-    showSpinner: false, // Disable the spinner
-  });
-
-  useEffect(() => {
-    const handleRouteStart = () => NProgress.start();
-    const handleRouteDone = () => NProgress.done();
-
-    Router.events.on('routeChangeStart', handleRouteStart);
-    Router.events.on('routeChangeComplete', handleRouteDone);
-    Router.events.on('routeChangeError', handleRouteDone);
-
-    return () => {
-      // Make sure to remove the event handler on unmount!
-      Router.events.off('routeChangeStart', handleRouteStart);
-      Router.events.off('routeChangeComplete', handleRouteDone);
-      Router.events.off('routeChangeError', handleRouteDone);
-    };
-  }, []);
+  useNProgress();
 
   return (
     <SessionProvider session={session}>
